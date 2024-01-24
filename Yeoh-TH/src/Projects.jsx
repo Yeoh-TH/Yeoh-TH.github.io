6b2a22eb1050fc2bfbee647699bc524ee3803edc
YeohTH <soundwavedecepticonleader@gmail.com>
2024-01-24 19:34:34 +0800

    Forgot to leave out projects

--- Yeoh-TH/src/Projects.jsx
+++ Yeoh-TH/src/Projects.jsx
@@ -9,7 +9,6 @@ function Projects(){
             <a className="navbar-button" href="#/">Homepage</a>
             <a className="navbar-button-active" href="#/projects">Projects</a>
             <a className="navbar-button" href="#/awards">Awards</a>
-            <a className="navbar-button" href="#/lilstories">Little Stories!</a>
         </div>
         <div id="timeline">
             <a className="yearDisplay" href="#/projects/2021">2021</a>
