3ec7e8e81978a4751ab3c3ca65bdfc8152f218a9
YeohTH <soundwavedecepticonleader@gmail.com>
2025-01-05 21:24:51 +0800

    Changed Aesthetic

--- Yeoh-TH/src/App.jsx
+++ Yeoh-TH/src/App.jsx
@@ -1,22 +1,23 @@
 import react from 'react'
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from "./Homepage";
 import Projects from "./Projects";
 import Projects2023 from './Projects2023';
 import Projects2022 from './Projects2022';
 import Awards from "./Awards";
 
-const App = () =>{
-  return(
-  <>
-  <Routes>
-    <Route path="/" exact element={<Homepage/>}/>
-    <Route path="/projects" element={<Projects/>}/>
-    <Route path="/projects/2022" element={<Projects2022/>}/>
-    <Route path="/projects/2023" element={<Projects2023/>}/>
-    <Route path="/awards" element={<Awards/>}/>
-  </Routes>
-  </>
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/2021" element={<Projects2021 />} />
+        <Route path="/projects/2022" element={<Projects2022 />} />
+        <Route path="/projects/2023" element={<Projects2023 />} />
+        <Route path="/awards" element={<Awards />} />
+      </Routes>
+    </>
   )
 }
 
--- Yeoh-TH/src/Homepage.jsx
+++ Yeoh-TH/src/Homepage.jsx
@@ -2,43 +2,38 @@ import React from "react";
 import developerLogo from "/THDeveloperLogomark2.png";
 import "./index.css";
 
-function Homepage(){
-    return(
+function Homepage() {
+    return (
         <>
-        <div id="navbar">
-            <a className="navbar-button-active" href="#/">Homepage</a>
-            <a className="navbar-button" href="#/projects">Projects</a>
-            <a className="navbar-button" href="#/awards">Awards</a>
-        </div>
-        <h1>
-        <span className="hoverEffect">W</span>
-        <span className="hoverEffect">e</span>
-        <span className="hoverEffect">l</span>
-        <span className="hoverEffect">c</span>
-        <span className="hoverEffect">o</span>
-        <span className="hoverEffect">m</span>
-        <span className="hoverEffect">e</span>
-        <span className="hoverEffect">!</span>
-        </h1>
-        <div className="paragraph">
-        <img src={developerLogo} className="logo"></img>
-        <p className="paragraphText">I am Tian Huai, or otherwise known as Yeoh-TH, and I hope that you will enjoy navigating this pretty simple website to learn more about me. Cheers!</p>
-        </div>
-        <div className="paragraph">
-            <h3 className="paragraphText">My personal email:
-            <br/>
-            <a href="mailto:yeohth231@gmail.com"><i class="fas fa-envelope" style={{fontSize:"6vw"}}></i></a>
-            <span style={{marginLeft:"1vw"}}>yeohth231@gmail.com</span>
-            </h3>
-            <h3 className="paragraphText" style={{marginLeft:"1vw"}}> My Youtube Channel:
-                <br/>
-                <a href="https://www.youtube.com/channel/UCiu6IweqcO0erslTDBVzwkg" target="_blank"><i class="fab fa-youtube"style={{fontSize:"8vw",color:"#e15634"}}></i></a>
-            </h3>
-            <h3 className="paragraphText" style={{marginLeft:"1vw"}}>My instagram:
-            <br/>
-            <a href="https://www.instagram.com/yeoh_th/" target="_blank"><i class="fab fa-instagram" style={{fontSize:"7vw",color:"#593f62"}}></i></a>
-            </h3>
-        </div>
+            <div id="navbar">
+                <a className="navbar-button-active" href="#/">Homepage</a>
+                <a className="navbar-button" href="#/projects">Projects</a>
+                <a className="navbar-button" href="#/awards">Awards</a>
+            </div>
+            <h1>
+                <span className="hoverEffect">T</span>
+                <span className="hoverEffect">i</span>
+                <span className="hoverEffect">a</span>
+                <span className="hoverEffect">n</span>
+                <span className="hoverEffect"> </span>
+                <span className="hoverEffect">H</span>
+                <span className="hoverEffect">u</span>
+                <span className="hoverEffect">a</span>
+                <span className="hoverEffect">i</span>
+            </h1>
+            <div className="intro">
+                <h2 id="introPara">The Introduction</h2>
+                <p className="introText">Turmoil has engulfed the world. In this Volatile, Uncertain, Complex and Ambiguous world, one has to learn many skills to help oneself get above the rest and achieve success.
+                    <br />Thus, the campaigner, YEOH TIAN HUAI, has decided to embark on this perilous journey, gaining multiple friends, valuable experiences, and most importantly memories.
+                    Herein lies all his stories from his multiple endeavours. Enjoy!
+                </p>
+            </div>
+            {/* <div className="paragraph">
+                <h3 className="paragraphText" style={{ marginLeft: "1vw" }}>My instagram:
+                    <br />
+                    <a href="https://www.instagram.com/yeoh_th/" target="_blank"><i class="fab fa-instagram" style={{ fontSize: "7vw", color: "#593f62" }}></i></a>
+                </h3>
+            </div> */}
         </>
     )
 }
